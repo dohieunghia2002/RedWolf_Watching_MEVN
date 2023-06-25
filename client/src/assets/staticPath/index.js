28a9128e966a7c13ee5f5b56dce9f709d90cb35b
dohieunghia2002 <jeetkunedo10000@gmail.com>
2023-06-25 10:06:22 +0700

    web app watching movies

--- client/src/assets/staticPath/index.js
+++ client/src/assets/staticPath/index.js
@@ -1,11 +1,3 @@
-function pathImgMedia() {
-    return '/src/assets/images/media/'
-}
-
-function pathImgCast() {
-    return '/src/assets/images/casts/'
-}
-
 function pathImgMember() {
     return '/src/assets/images/member/'
 }
